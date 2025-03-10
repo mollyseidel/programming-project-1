130009d2fce02c20f98757b652915e9f32b56d03
19662005 <claire_piron@brown.edu>
2025-03-10 13:48:26 -0400

    q1

--- question1.m
+++ question1.m
@@ -3,17 +3,19 @@ num_participants = 5; %to be changed
 responses = zeros(num_participants, 1);  % Creating a column vector to store responses
 
 for i = 1:num_participants
-% Ask to rate how directly they feel climate change is impacting them
-perceived_impact = input('How directly do you feel climate change is impacting your life or livelihood? (1 = not at all, 10 = immediate threat): ');
+% Ask to rate if they feel impacted by climate change
+climate_impact = input('How directly do you feel climate change is impacting your life or livelihood? (1 - 10 scale: 1 = not at all, 10 = immediate threat): ');
 
 % Make sure response is between 1 and 10
-while perceived_impact < 1 || perceived_impact > 10
+while climate_impact < 1 || climate_impact > 10
     disp('Please enter a number between 1 and 10.');
-    perceived_impact = input('How directly do you feel climate change is impacting your life or livelihood? (1 = not at all, 10 = immediate threat): ');
+    climate_impact = input('If the opportunity arose, would you involve yourself with advocacy for climate change mitigation or adaptation? (1 = not at all, 10 = immediate threat): ');
 end
 
-responses(i) = perceived_impact; %not working
+responses(i) = advocacy; %not working
 
+disp(responses)
 end
 
-save('question1.m', 'responses'); %saving for later calculations
+save('question1.m', 'responses'); %saving for later calculations - I'm not displaying them but I want to store them for later, like a column vector to later use maybe? 
+
