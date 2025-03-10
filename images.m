f49b02b940ece7bcc9259727465dc093a467a8d6
mollyseidel <molly_seidel@brown.edu>
2025-03-10 14:29:21 -0400

    m

--- images.m
+++ images.m
@@ -1,1 +1,7 @@
 %images
+%clear all screens and figures as well as all variables in the workspace
+sca;
+close all;
+clear all;
+%call defauly settings for setting up Psychtoolbox
+PsychDefaultSetup(2);
