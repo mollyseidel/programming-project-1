6d651d6790ce7c70411a000a56eec0ac7610f545
mollyseidel <molly_seidel@brown.edu>
2025-03-10 14:38:04 -0400

    establishing grey background for images

--- images.m
+++ images.m
@@ -5,3 +5,16 @@ close all;
 clear all;
 %call defauly settings for setting up Psychtoolbox
 PsychDefaultSetup(2);
+
+
+%establishing a screen with background grey
+screens = Screen ('Screens');
+
+screenNumber = max(screens);
+white = WhiteIndex (screenNumber);
+black = BlackIndex (screenNumber);
+grey = white/2;
+
+[window, windowRect] = PsychImaging ('OpenWindow', screenNumber, grey);
+KbStrokeWait;
+sca;
