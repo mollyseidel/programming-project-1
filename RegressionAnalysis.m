590c449beb6eb2af6dfe2da758da39a9da5b37e2
19662005 <claire_piron@brown.edu>
2025-03-11 20:14:08 -0400

    regression analysis attempt

--- RegressionAnalysis.m
+++ RegressionAnalysis.m
@@ -4,3 +4,11 @@ disp(responses)
 
 load("question1responses.mat");
 disp(responses)
+
+X = [images,"question1responses.mat", ]; %change when images get set
+
+Y = "question2responses.mat";
+
+mdl = fitlm(X,Y);
+
+disp(mdl)
