6ce04eca3c2519cb1301a428164c58cfdf154dd4
19662005 <claire_piron@brown.edu>
2025-03-18 20:26:04 -0400

    debugging

--- RegressionAnalysis.m
+++ RegressionAnalysis.m
@@ -33,8 +33,8 @@ disp(mdl);
 disp(mdl1);
 
 % Extracting coefficients, p-values, and R-squared value
-coefficients = mdl.Coefficients.Estimate;  % Should be 3 Coefficients for the 'predictors'
-pValues = mdl.Coefficients.pValue;         % Should be 3 P-values for the 'predictors'
+coefficients = mdl.Coefficients.Estimate;  % Should be 2 Coefficients for the 'predictors'
+pValues = mdl.Coefficients.pValue;         % Should be 2 P-values for the 'predictors'
 rSquared = mdl.Rsquared.Ordinary;          % R-squared value
 
 acceptHypothesis1 = true;
@@ -43,17 +43,17 @@ acceptHypothesis1 = true;
 if any(coefficients <= 0)
     acceptHypothesis1 = false;  % Reject if any coefficient is negative
 end
-
+disp(coefficients)
 % Check if all p-values are less than 0.05
 if any(pValues >= 0.05)
     acceptHypothesis1 = false;  % Reject if any p-value is >= 0.05
 end
-
+disp(pValues)
 % Check if R-squared is above 0.70
 if rSquared <= 0.70
     acceptHypothesis1 = false;  % Reject if R-squared is <= 0.70
 end
-
+disp(rSquared)
 % Final decision
 if acceptHypothesis1
     disp('People who perceive themselves to be more directly impacted by climate change are more likely to support mitigation efforts');
@@ -63,24 +63,24 @@ end
 
 
 % Extracting coefficients, p-values, and R-squared value
-coefficients = mdl1.Coefficients.Estimate;  % Should be 3 Coefficients for the 'predictors'
-pValues = mdl1.Coefficients.pValue;         % Should be 3 P-values for the 'predictors'
-rSquared = mdl1.Rsquared.Ordinary;          % R-squared value
+coefficients1 = mdl1.Coefficients.Estimate;  % Should be 3 Coefficients for the 'predictors'
+pValues1 = mdl1.Coefficients.pValue;         % Should be 3 P-values for the 'predictors'
+rSquared1 = mdl1.Rsquared.Ordinary;          % R-squared value
 
 acceptHypothesis2 = true;
 
 % Check if all coefficients are positive
-if any(coefficients <= 0)
+if any(coefficients1 <= 0)
     acceptHypothesis2 = false;  % Reject if any coefficient is negative
 end
 
 % Check if all p-values are less than 0.05
-if any(pValues >= 0.05)
+if any(pValues1 >= 0.05)
     acceptHypothesis2 = false;  % Reject if any p-value is >= 0.05
 end
 
 % Check if R-squared is above 0.70
-if rSquared <= 0.70
+if rSquared1 <= 0.70
     acceptHypothesis2 = false;  % Reject if R-squared is <= 0.70 
 end
 
