019c588df571346ef24fdd3e15b200845682f369
19662005 <claire_piron@brown.edu>
2025-03-20 15:55:11 -0400

    deletingextra

--- zscoring.m
+++
@@ -1,9 +0,0 @@
-
-
-subjA = [1,2,3,4];
-subjB = [6,7,8,8];
-subjC = [1,2,1,2];
-
-subjA = (subjA - mean(subjA)) / std(subjA);
-subjB = (subjB - mean(subjB)) / std(subjB);
-subjC = (subjC - mean(subjC)) / std(subjC);
