9927e13ae9454b0a58906f1f9752defa6b1c137b
19662005 <claire_piron@brown.edu>
2025-03-20 15:53:15 -0400

    30participants

--- question1TEST.m
+++ question1TEST.m
@@ -1,5 +1,5 @@
 
-subid= 0013; %change for each participant
+subid= 0030; %change for each participant
 num_participants = 2;
 
 responses = zeros(num_participants, 1);  % Creating a column vector to store responses
